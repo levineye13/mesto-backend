b087cd0452c8448377272eea3e9c555f2be96b8d
Oleg Zharov <levineye@gmail.com>
2020-11-26 16:11:15 +0300

    feat: добавить статические файлы

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+//app.get("/users", (req, res) => {});
+
+app.use(express.static("public"));
+
 app.listen(PORT, () => {
   console.log("Port " + PORT);
 });
