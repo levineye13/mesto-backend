e423f192d6cdbbfabcde0be0d8f778d0624b8a69
Oleg Zharov <levineye@gmail.com>
2020-11-26 23:54:59 +0300

    feat: добавить статические файлы проекта mesto-react

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-//app.get("/users", (req, res) => {});
+const users = require('./data/users.json');
+const cards = require('./data/cards.json');
 
-app.use(express.static("public"));
+app.get('/users', (req, res) => {
+  res.status(200).send(users);
+});
+
+app.get('/cards', (req, res) => {
+  res.status(200).send(cards);
+});
+
+app.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!users[id]) {
+    res
+      .status(400)
+      .send(JSON.stringify({ message: 'Нет пользователя с таким id' }));
+    return;
+  }
+
+  res.send(users[id]);
+});
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(400)
+//     .send(JSON.stringify({ message: 'Запрашиваемый ресурс не найден' }));
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log("Port " + PORT);
+  console.log('Port ' + PORT);
 });
