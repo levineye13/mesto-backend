efc2d5e644a99e4da39fc07f899020b4a4a7089d
Oleg Zharov <levineye@gmail.com>
2020-12-09 15:09:34 +0300

    feat: добавить пакет mongoose и подключиться к бд

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
 const { cardsRouter } = require('./routes/cards.js');
 const { usersRouter } = require('./routes/users.js');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', cardsRouter);
